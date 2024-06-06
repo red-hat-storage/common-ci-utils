589dde7bfeb7d5a345588f3f39cda2a60b0fd9e0
Petr Balogh <pbalogh@redhat.com>
2024-06-06 11:23:06 +0200

    Improve download_rpm function to add auth and stream method
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- common_ci_utils/package_fetcher.py
+++ common_ci_utils/package_fetcher.py
@@ -10,12 +10,14 @@ import tempfile
 log = logging.getLogger(__name__)
 
 
-def download_rpm(rpm_url):
+def download_rpm(rpm_url, username=None, password=None):
     """
     Downloads RPM
 
     Args:
         rpm_url (str): RPM URL to download
+        username (str): username (optional when authentication is required)
+        password (str): password (optional when authentication is required)
 
     Returns:
         str: Path to RPM file
@@ -30,15 +32,18 @@ def download_rpm(rpm_url):
         mode="w+", prefix=f"{rpm_name}_", suffix=f"{rpm_ext}", delete=False
     )
     rpm_file_name = rpm_file.name
-
+    auth = None
+    if username and password:
+        auth = (username, password)
     try:
         # Send an HTTP GET request to download the RPM package
-        response = requests.get(rpm_url)
+        response = requests.get(rpm_url, auth=auth, stream=True)
         response.raise_for_status()
 
         # Open a local file and write the RPM content to it
         with open(rpm_file_name, "wb") as f:
-            f.write(response.content)
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
         log.info(f"Downloaded RPM package to {rpm_file_name}")
         return rpm_file_name
 
